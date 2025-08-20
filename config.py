4dc6b70fdaa254257827388adca9f7bb9aa45d95
Lim Wee Yang <weeyang1489@gmail.com>
2025-08-20 17:47:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cer4su17lshh.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "limweeyang-employee"
 customregion = "us-east-1"
 
